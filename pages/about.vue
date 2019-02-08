baeab6315bb10aecdb7db6ebdeb2706809dd5a0c
Mohammad Kabir <mohammadkabir@gmail.com>
2019-02-07 21:43:05 -0600

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,40 @@
 <template>
+
+
   <div class="category">
   
-  About me.!
+ 
+ Over 20 years of Software Engineering experience in designing, analyzing, programming, maintaining, managing web based, distributed as well as client server Software Applications in multi-tier environment.
+
+TECHNICAL SKILLS:
+
+Business Intelligence Tools: Power BI, Tableau 2018, SQL Server, Salesforce, SSRS, Crystal Report
+
+ERP:	Infor SyteLIne
+
+Languages:	C#, VB .NET
+
+.NET Technologies:	ASP .NET MVC 5/4, ASP.NET CORE 2.1/2.0, ASP .NET Webform, Entity Framework CORE, ADO .NET, WPF, WCF, Winform, LINQ, Dapper
+
+Other Web Technologies:	Single Page Architecture (SPA), HTML 5, HTML, CSS, Bootstrap, LESS, SASS, XML, JSon
+
+Javascript Frameworks:	Vue.js, Angular JS, Angular, React, JQuery, AJAX, node.js,
+
+SOA Technologies:	Restful Web API, WCF, Web Services, ASMX, Microservices, Enterprise Service Bus, Kubernetes, Postman, Swagger, Stackify, Fiddler
+
+RDBMS:	SQL Server, Oracle, MySQL
+
+Technical Architecture:	MVC, MVVM, MVP, Design patterns such as Repository, Factory, Singleton, Façade, Iterator, Observatory, UML, OOD, n-tier architecture
+
+Source Control:	VSTS, SVN, Github, Bitbucket, TFS, Continuous Integration, Continuous Delivery, Docker, Test Driven Development (TDD)
+
+Cloud Technologies:	Azure, Azure App Fabric, Amazon AWS
+
+Integration/Unit Test:	XUnit, NUnit, MsBuild
+
+Tools:	Visual Studio 2017, Telerik, extJS, Power BI, Tabelau 2018
+
+SDLC:	Agile, Waterfall, Jira, Confluence, Scrum, WBS 
   
     <!--<nuxt-affix :list="list" category="guide" />-->
     
