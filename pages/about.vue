fdc6b62288fce63ba72e88fd852d4c6b2f36e8aa
Mohammad Kabir <mohammadkabir@gmail.com>
2019-02-09 20:06:29 -0600

    2/9/2019

--- pages/about.vue
+++ pages/about.vue
@@ -2,49 +2,55 @@
 
 
   <div class="category">
-  
- 
- Over 20 years of Software Engineering experience in designing, analyzing, programming, maintaining, managing web based, distributed as well as client server Software Applications in multi-tier environment.
 
-TECHNICAL SKILLS:
 
-Business Intelligence Tools: Power BI, Tableau 2018, SQL Server, Salesforce, SSRS, Crystal Report
+    Over 20 years of Software Engineering experience in designing, analyzing, programming, maintaining, managing web based,
+    distributed as well as client server Software Applications in multi-tier environment.
+    <br />
 
-ERP:	Infor SyteLIne
+    <strong>TECHNICAL SKILLS:</strong><br />
 
-Languages:	C#, VB .NET
+    Business Intelligence Tools: Power BI, Tableau 2018, SQL Server, Salesforce, SSRS, Crystal Report<br />
 
-.NET Technologies:	ASP .NET MVC 5/4, ASP.NET CORE 2.1/2.0, ASP .NET Webform, Entity Framework CORE, ADO .NET, WPF, WCF, Winform, LINQ, Dapper
+    ERP:	Infor SyteLIne<br />
 
-Other Web Technologies:	Single Page Architecture (SPA), HTML 5, HTML, CSS, Bootstrap, LESS, SASS, XML, JSon
+    Languages:	C#, VB .NET<br />
 
-Javascript Frameworks:	Vue.js, Angular JS, Angular, React, JQuery, AJAX, node.js,
+    .NET Technologies:	ASP .NET MVC 5/4, ASP.NET CORE 2.1/2.0, ASP .NET Webform, Entity Framework CORE, ADO .NET, WPF,
+    WCF, Winform, LINQ, Dapper <br />
 
-SOA Technologies:	Restful Web API, WCF, Web Services, ASMX, Microservices, Enterprise Service Bus, Kubernetes, Postman, Swagger, Stackify, Fiddler
+    Other Web Technologies:	Single Page Architecture (SPA), HTML 5, HTML, CSS, Bootstrap, LESS, SASS, XML, JSon<br />
 
-RDBMS:	SQL Server, Oracle, MySQL
+    Javascript Frameworks:	Vue.js, Angular JS, Angular, React, JQuery, AJAX, node.js,<br />
 
-Technical Architecture:	MVC, MVVM, MVP, Design patterns such as Repository, Factory, Singleton, Façade, Iterator, Observatory, UML, OOD, n-tier architecture
+    SOA Technologies:	Restful Web API, WCF, Web Services, ASMX, Microservices, Enterprise Service Bus,
+    Kubernetes, Postman, Swagger, Stackify, Fiddler<br />
 
-Source Control:	VSTS, SVN, Github, Bitbucket, TFS, Continuous Integration, Continuous Delivery, Docker, Test Driven Development (TDD)
+    RDBMS:	SQL Server, Oracle, MySQL<br />
 
-Cloud Technologies:	Azure, Azure App Fabric, Amazon AWS
+    Technical Architecture:	MVC, MVVM, MVP, Design patterns such as Repository, Factory, Singleton, Façade,
+    Iterator, Observatory, UML, OOD, n-tier architecture <br />
 
-Integration/Unit Test:	XUnit, NUnit, MsBuild
+    Source Control:	VSTS, SVN, Github, Bitbucket, TFS, Continuous Integration, Continuous Delivery, Docker,
+    Test Driven Development (TDD) <br />
 
-Tools:	Visual Studio 2017, Telerik, extJS, Power BI, Tabelau 2018
+    Cloud Technologies:	Azure, Azure App Fabric, Amazon AWS <br />
 
-SDLC:	Agile, Waterfall, Jira, Confluence, Scrum, WBS 
-  
+    Integration/Unit Test:	XUnit, NUnit, MsBuild <br />
+
+    <strong>Tools:	</strong>Visual Studio 2017, Telerik, extJS, Power BI, Tabelau 2018
+    <br />
+
+    SDLC:	Agile, Waterfall, Jira, Confluence, Scrum, WBS <br />
+
     <!--<nuxt-affix :list="list" category="guide" />-->
-    
     <!--
-    <div class="category__content Content Content--hasCarbon" :class="{'category__content--hidden': visible}">
-      <nuxt-child/>
-    </div>
-    -->
+  <div class="category__content Content Content--hasCarbon" :class="{'category__content--hidden': visible}">
+    <nuxt-child/>
+  </div>
+  -->
     <div class="category__footer">
-      <nuxt-footer/>
+      <nuxt-footer />
     </div>
   </div>
 </template>
